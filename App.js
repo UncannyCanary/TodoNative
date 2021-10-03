7604deb8d0fb9a59ea8965d0dc786fba4f30ecfb
Vishwanath Sah <nightwish@pop-os.localdomain>
2021-10-03 20:08:06 +0530

    Testing text Input

--- App.js
+++ App.js
@@ -1,12 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
+  const [name, setname] = useState("Vishwanath");
+  const [age, setage] = useState("24");
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Enter name:</Text>
+      <TextInput style={styles.input} 
+        placeholder="eg. Kory" 
+        onChangeText={(val) => setname(val)}
+        />
+
+      <Text>Enter age:</Text>
+      <TextInput style={styles.input} 
+        placeholder="eg. 20" 
+        onChangeText={(val) => setage(val)}
+        keyboardType="numeric"
+        />
+      <Text>
+        Hi my name is {name} and my age is {age}
+      </Text>
     </View>
   );
 }
@@ -14,8 +29,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#777",
+    padding: 5,
+    paddingLeft: 12,
+    margin: 5,
+    minWidth: 200,
   },
 });
