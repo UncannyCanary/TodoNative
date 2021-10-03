2a079a66d532000300f961d48dfb00dd95581ad7
Vishwanath Sah <nightwish@pop-os.localdomain>
2021-10-03 19:41:25 +0530

    Testing State

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const [nameStatus, setName] = useState(true);
+  const btnOnClick = () => {
+    setName(!nameStatus);
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Hi ! My name is {nameStatus ? `Vish` : `Nightwish`}</Text>
+      <View style={styles.buttonContainer}>
+        <Button title="Click Me !" onPress={btnOnClick}/>
+      </View>
     </View>
   );
 }
@@ -14,8 +20,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
+  buttonContainer: {
+    marginTop: 20
+  }
 });
