eebbeefa7bcec0a70a97dd34653d58e8db7c6fdc
Vishwanath Sah <nightwish@pop-os.localdomain>
2021-10-03 20:48:28 +0530

    FlatList new changes

--- App.js
+++ App.js
@@ -1,27 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
-import { FlatList, StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { FlatList, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   const [people, setpeople] = useState([
-    {name: 'Vish1', key: '1'},
-    {name: 'Vish2', key: '2'},
-    {name: 'Vish3', key: '3'},
-    {name: 'Vish4', key: '4'},
-    {name: 'Vish5', key: '5'},
-    {name: 'Vish6', key: '6'},
-    {name: 'Vish7', key: '7'},
-    {name: 'Vish8', key: '8'},
-    {name: 'Vish9', key: '9'},
-    {name: 'Vish10', key: '10'}
+    { name: "Vish1", id: "1" },
+    { name: "Vish2", id: "2" },
+    { name: "Vish3", id: "3" },
+    { name: "Vish4", id: "4" },
+    { name: "Vish5", id: "5" },
+    { name: "Vish6", id: "6" },
+    { name: "Vish7", id: "7" },
+    { name: "Vish8", id: "8" },
+    { name: "Vish9", id: "9" },
+    { name: "Vish10", id: "10" },
   ]);
   return (
     <View style={styles.container}>
       <FlatList
+        keyExtractor={(item) => item.id}
         data={people}
-        renderItem={({item}) => (
-          <View><Text style={styles.person}>{item.name}</Text></View>
-        )}  
+        renderItem={({ item }) => (
+          <View>
+            <Text style={styles.person}>{item.name}</Text>
+          </View>
+        )}
       />
     </View>
   );
@@ -30,8 +33,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    paddingHorizontal: 20,
+    paddingTop: 20,
+  },
+  person: {
+    marginTop: 24,
+    padding: 30,
+    backgroundColor: "pink",
+    fontSize: 24,
   },
 });
