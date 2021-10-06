b28cd19c4b136deb72c10ae9204f65f24e677eb1
Vishwanath Sah <nightwish@pop-os.localdomain>
2021-10-06 23:50:56 +0530

    ToDoList

--- App.js
+++ App.js
@@ -1,21 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { Alert, FlatList, Keyboard, StyleSheet, Text, TouchableWithoutFeedback, View } from "react-native";
+import AddTodo from "./Components/AddTodo";
+import Header from "./Components/Header";
+import TodoItem from "./Components/TodoItem";
 
 export default function App() {
+  const [todos, settodos] = useState([
+    { text: "buy coffee", key: "1" },
+    { text: "create an app", key: "2" },
+    { text: "play paladins", key: "3" },
+  ]);
+
+  const submitHandler = (text) => {
+    if (text.length > 3) {
+      settodos((prevState) => {
+        return [{ text: text, key: Math.random().toString() }, ...prevState];
+      });
+    }
+    else {
+      Alert.alert('OOPS !', 'Todos must be over 3 characters long', [{text : 'Understood', onPress() {console.log("Alert Closed !")}}]);
+    }
+  };
+
+  const pressHandler = (key) => {
+    settodos((prevTodos) => {
+      return prevTodos.filter((todo) => todo.key != key);
+    });
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <TouchableWithoutFeedback onPress={() => {
+      Keyboard.dismiss();
+    }}>
+      <View style={styles.container}>
+        <Header />
+        <View style={styles.content}>
+          <AddTodo submitHandler={submitHandler} />
+          <View style={styles.list}>
+            <FlatList
+              data={todos}
+              renderItem={({ item }) => (
+                <TodoItem item={item} pressHandler={pressHandler} />
+              )}
+            />
+          </View>
+        </View>
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+  },
+  content: {
+    padding: 40,
+  },
+  list: {
+    marginTop: 20,
   },
 });
